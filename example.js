3951b69e735f0c06a155576b78ed2130a9057768
Joseph Okpara <joejr.okpara@gmail.com>
2018-10-13 09:38:03 -0500

    Working javascript program

---
+++ example.js
@@ -0,0 +1,9 @@
+var message = "Hello World: This is a statement"
+console.log(message + " " + 1);
+console.log(message + " " + 2);
+console.log(message + " " + 3);
+console.log(message + " " + 4);
+console.log(message + " " + 5);
+console.log(message + " " + 6);
+console.log(message + " " + 7);
+console.log(message + " " + 8);
